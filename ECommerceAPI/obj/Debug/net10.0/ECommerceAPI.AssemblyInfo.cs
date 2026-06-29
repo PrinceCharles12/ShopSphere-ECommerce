82027cda98b7bad5076b11da505a4b8c9bb8e234
Prince Sharma <sharmajiprince2003@gmail.com>
2026-06-29 12:00:50 +0530

    Ignore build folders

--- ECommerceAPI/obj/Debug/net10.0/ECommerceAPI.AssemblyInfo.cs
+++ ECommerceAPI/obj/Debug/net10.0/ECommerceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cee3b87b92dd9ab2afef3739913236608eebb66")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
